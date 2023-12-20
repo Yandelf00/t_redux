841eb345fb4a099489848c1c85135d3814312382
Yandelf00 <125671903+Yandelf00@users.noreply.github.com>
2023-12-20 22:12:51 +0100

    localstorage

--- store/index.tsx
+++ store/index.tsx
@@ -1,8 +1,22 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 
+
+const getInitialState = () => {
+    const isServer = typeof window === 'undefined';
+    
+    // If it's the server, return a default state
+    if (isServer) {
+      return { counter: 0 };
+    }
+  
+    // If it's the client, retrieve the state from local storage
+    const storedState = localStorage.getItem("counterState");
+    return storedState ? JSON.parse(storedState) : { counter: 0 };
+};
+
 const counterSlice = createSlice({
     name : 'counter',
-    initialState : {counter : 0},
+    initialState : getInitialState(),
     reducers : {
         increment(state, action) {
             state.counter++;
@@ -18,5 +32,9 @@ export const actions = counterSlice.actions;
 const store = configureStore({
     reducer: counterSlice.reducer
 })
+
+store.subscribe(() => {
+    localStorage.setItem("counterState", JSON.stringify(store.getState()));
+});
 export default store;
 
